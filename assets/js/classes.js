2fd85c28d2c2a3131a34a45265d5e24ac03e59a5
Ben <benjustusbals@gmail.com>
2014-11-21 20:08:51 +0100

    you can attack now

--- assets/js/classes.js
+++ assets/js/classes.js
@@ -1,33 +1,63 @@
 function Fight (enemy, enemy_lvl) {
-  this.character = localStorage.character;
-  this.player_lvl = parseInt(localStorage.lvl);
-  this.enemy = enemy;
-  this.enemy_lvl = enemy_lvl;
+	this.character = localStorage.character;
+	this.player_lvl = parseInt(localStorage.lvl);
+	this.enemy = enemy;
+	this.enemy_lvl = enemy_lvl;
 
-  //Keeps track of health
-  this.player_health = Math.round(parseInt(data.creatures[this.character].basestats.hp) * parseInt(localStorage.lvl) / 33);
-  this.enemy_health = Math.round(parseInt(data.creatures[this.enemy].basestats.hp) * parseInt(enemy_lvl) / 33);
-  this.player_basehealth = this.player_health
-  this.enemy_basehealth = this.enemy_health
+	//Keeps track of health
+	this.player_health = Math.round(parseInt(data.creatures[this.character].basestats.hp) * parseInt(localStorage.lvl) / 33);
+	this.enemy_health = Math.round(parseInt(data.creatures[this.enemy].basestats.hp) * parseInt(enemy_lvl) / 33);
+	this.player_basehealth = this.player_health;
+	this.enemy_basehealth = this.enemy_health;
 
-  //Keeps track type and multipliers
-  this.player_type = data.creatures[this.character].basestats.type;
-  this.enemy_type = data.creatures[this.enemy].basestats.type;
-  this.player_multiplier = 1.0;
-  this.enemy_multiplier = 1.0;
+	//Keeps track type and multipliers and attacks
+	this.player_type = data.creatures[this.character].basestats.type;
+	this.enemy_type = data.creatures[this.enemy].basestats.type;
 
-  this.update_health = function () {
-    $(".fight_area > .creatures > .player > paper-progress").attr("value", Math.round(this.player_health / this.player_basehealth * 100))
-    $(".fight_area > .creatures > .enemy > paper-progress").attr("value", Math.round(this.enemy_health / this.enemy_basehealth * 100))
-  };
+	this.player_attack_multiplier = 1.0;
+	this.enemy_attack_multiplier = 1.0;
+	this.player_defense_multiplier = 1.0;
+	this.enemy_defense_multiplier = 1.0 ;
 
-  this.set_display = function () {
-    //setting h1 with names and lvl of creatures
-    $('.fight_area > .creatures > .player > h1 > span.name').html(this.character);
-    $('.fight_area > .creatures > .enemy > h1 > span.name').html(this.enemy);
-    $('.fight_area > .creatures > .player > h1 > span.lvl > span').html(this.player_lvl);
-    $('.fight_area > .creatures > .enemy > h1 > span.lvl > span').html(this.enemy_lvl);
+	this.player_attacks = data.creatures[this.character].attacks;
+	this.enemy_attacks = data.creatures[this.enemy].attacks;
 
-    this.update_health();
-  };
+	//keeps track of whos turn it is
+	this.turn = 'player';
+	this.victim = 'enemy';
+
+	this.switch_turn = function () {
+		if (this.turn == 'player') {
+			this.turn = 'enemy';
+			this.victim = 'player';
+			$(".select_attack > .attack_btns > paper-button").attr("disabled", "");
+		} else {
+			this.turn = 'player';
+			this.victim  = 'enemy';
+			$(".select_attack > .attack_btns > paper-button").removeAttr("disabled");
+		}
+	};
+
+	this.update_health = function () {
+		$(".fight_area > .creatures > .player > paper-progress").attr("value", Math.round(this.player_health / this.player_basehealth * 100));
+		$(".fight_area > .creatures > .enemy > paper-progress").attr("value", Math.round(this.enemy_health / this.enemy_basehealth * 100));
+	};
+
+	this.set_display = function () {
+		//setting h1 with names and lvl of creatures
+		$('.fight_area > .creatures > .player > h1 > span.name').html(this.character);
+		$('.fight_area > .creatures > .enemy > h1 > span.name').html(this.enemy);
+		$('.fight_area > .creatures > .player > h1 > span.lvl > span').html(this.player_lvl);
+		$('.fight_area > .creatures > .enemy > h1 > span.lvl > span').html(this.enemy_lvl);
+
+		//Attack btns
+		$(".select_attack > .attack_btns").html("");
+		for (var i = 0; i < this.player_attacks.length; i++) {
+			$(".select_attack > .attack_btns").append(
+				"<paper-button onclick='fight_attack("+ this.player_attacks[i]+ ")'>" + this.player_attacks[i] + "</paper-button>"
+			);
+		}
+
+		this.update_health();
+	};
 }
--- assets/js/fight.coffee
+++ assets/js/fight.coffee
@@ -4,4 +4,36 @@ fight_start = (enemy, enemy_lvl) ->
   currentFight.set_display()
 
 fight_attack = (attack_name) ->
-  console.log data[attack_name]
+  currAttack = data.attacks[attack_name]
+
+  console.log currAttack
+
+  #actual changing of values
+  if currAttack.action[0] is 'hp'
+    console.log "It's a hp action and it takes " + Math.round(parseInt(currAttack.action[1]) * currentFight[currentFight.turn + '_attack_multiplier'] * currentFight[currentFight.victim + '_defense_multiplier'] * currentFight[currentFight.turn + '_lvl'] / 33)
+    currentFight[currentFight.victim + '_health'] += Math.round(parseInt(currAttack.action[1]) * currentFight[currentFight.turn + '_attack_multiplier'] * currentFight[currentFight.victim + '_defense_multiplier'] * currentFight[currentFight.turn + '_lvl'] / 33)
+    currentFight.update_health()
+
+  #output
+  if currentFight.turn is 'player'
+    notify fight_parse_inline_vars(currAttack.text)
+  else if currentFight.turn is 'enemy'
+    swal fight_parse_inline_vars(currAttack.text)
+    notify fight_parse_inline_vars(currAttack.text)
+  else
+    console.log "No fight has been start yet or currentFight.turn is wrong: #{currentFight.turn}"
+
+fight_parse_inline_vars = (text) ->
+  __attacker = currentFight.character
+  __victim = currentFight.enemy
+  if currentFight.turn is 'enemy'
+    __attacker = currentFight.enemy
+    __victim = currentFight.character
+    
+  __res = text.split("||")
+  for i in [0..__res.length-1]
+  	if __res[i].toLowerCase() is 'creature'
+  	  __res[i] = __attacker
+    else if __res[i].toLowerCase() is 'enemy'
+      __res[i] = __victim
+  __res = __res.join("") 
